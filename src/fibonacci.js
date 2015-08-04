46ebbe3c62b29b01b91d32d4717b29ea57c456ae
Justin Tormey <jrtormey@gmail.com>
2015-08-04 15:45:37 -0400

    Solution for fibonacci.js

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,16 @@
 
 function fibonacci (n) {
   // recursively compute the nth fibonacci number
+
+  // First specify the edge cases
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  // Ask for the sum of the previous two fibonacci numbers
+  return fibonacci(n-1) + fibonacci(n-2);
 }
+
+var result = fibonacci(6); // result = 13;
+console.log(result);
 
